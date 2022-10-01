09eae2c03be840fadc41e595373ec949f0710286
ot07 <ot07neoki@gmail.com>
2022-10-01 19:48:39 +0900

    ✨ detect whether page is source code page or not

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,11 +1,27 @@
-import { createRoot } from "react-dom/client";
-import App from "@src/pages/content/components/Demo/app";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
 
 refreshOnUpdate("pages/content/components/Demo");
 
 const root = document.createElement("div");
 root.id = "github-indent-rainbow-content-view-root";
-document.body.append(root);
+document.body.prepend(root);
 
-createRoot(root).render(<App />);
+const onUpdate = () => {
+  const fileLineContainer = document.getElementsByClassName(
+    "js-file-line-container"
+  );
+
+  root.innerText = fileLineContainer.length
+    ? "This is a source code page (content view)"
+    : "This is NOT a source code page (content view)";
+};
+
+const urlChangeObserver = new MutationObserver(() => {
+  console.log("DOM changed!");
+  urlChangeObserver.disconnect();
+  onUpdate();
+  urlChangeObserver.observe(document, { childList: true, subtree: true });
+});
+
+onUpdate();
+urlChangeObserver.observe(document, { childList: true, subtree: true });
