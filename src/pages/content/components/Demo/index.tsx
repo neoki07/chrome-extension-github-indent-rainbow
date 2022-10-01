c338efe974b02dff77970aa1e5ea4ad296014b07
ot07 <ot07neoki@gmail.com>
2022-10-02 02:08:05 +0900

    ✨ detect indent

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,17 +1,47 @@
+import detectIndent from "detect-indent";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
 
 refreshOnUpdate("pages/content/components/Demo");
+
+const colors = [
+  "rgba(255,255,64,0.07)",
+  "rgba(127,255,127,0.07)",
+  "rgba(255,127,255,0.07)",
+  "rgba(79,236,236,0.07)",
+];
+
+const errorColor = "rgba(128,32,32,0.6)";
+
+const tabmixColor = "rgba(128,32,96,0.6)";
 
 const root = document.createElement("div");
 root.id = "github-indent-rainbow-content-view-root";
 document.body.prepend(root);
 
-const onUpdate = () => {
+const fetchGithubContent = async (
+  repo: string,
+  branch: string,
+  path: string
+): Promise<any> => {
+  return await fetch(
+    encodeURI(`https://raw.githubusercontent.com/${repo}/${branch}/${path}`)
+  ).then((response) => response.text());
+};
+
+const onUpdate = async () => {
   const fileLineContainers = document.getElementsByClassName(
     "js-file-line-container"
   );
 
   if (fileLineContainers.length) {
+    const url = location.href;
+    const splitUrl = url.split("/");
+    const repo = `${splitUrl[3]}/${splitUrl[4]}`;
+    const branch = splitUrl[6];
+    const path = splitUrl.slice(7).join("/");
+    const content = await fetchGithubContent(repo, branch, path);
+    const indentSize = detectIndent(content).amount;
+
     const fileLines =
       fileLineContainers[0].getElementsByClassName("js-file-line");
 
