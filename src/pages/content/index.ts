5d3db867a770ff29096b0941faa1d6a35a7b9c1a
ot07 <ot07neoki@gmail.com>
2022-10-20 00:19:36 +0900

    ♻️ refactor

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -188,6 +188,100 @@ const getLinesIndentGuides = (
   return result;
 };
 
+const getIndentLevelForWhitespaceLine_ = (
+  aboveContentLineIndent: number,
+  belowContentLineIndent: number
+): number => {
+  if (aboveContentLineIndent === -1 || belowContentLineIndent === -1) {
+    // At the top or bottom of the file
+    return 0;
+  } else if (aboveContentLineIndent < belowContentLineIndent) {
+    // we are inside the region above
+    return aboveContentLineIndent;
+  } else if (aboveContentLineIndent === belowContentLineIndent) {
+    // we are in between two regions
+    return belowContentLineIndent;
+  } else {
+    // we are inside the region that ends below
+    return belowContentLineIndent;
+  }
+};
+
+const getLinesIndentLevels = (
+  lines: string[],
+  indentSize: number,
+  tabSize: number
+) => {
+  const lineCount = getLineCount(lines);
+
+  const result: number[] = new Array<number>(lineCount);
+
+  let aboveContentLineIndex =
+    -2; /* -2 is a marker for not having computed it */
+  let aboveContentLineIndent = -1;
+
+  let belowContentLineIndex =
+    -2; /* -2 is a marker for not having computed it */
+  let belowContentLineIndent = -1;
+
+  for (let lineNumber = 1; lineNumber <= lineCount; lineNumber++) {
+    const line = lines[lineNumber - 1];
+
+    const resultIndex = lineNumber - 1;
+
+    const currentIndent = computeIndentLevel(line, tabSize);
+    if (currentIndent >= 0) {
+      // This line has content (besides whitespace)
+      // Use the line's indent
+      aboveContentLineIndex = lineNumber - 1;
+      aboveContentLineIndent = currentIndent;
+      result[resultIndex] = currentIndent;
+
+      continue;
+    }
+
+    if (aboveContentLineIndex === -2) {
+      aboveContentLineIndex = -1;
+      aboveContentLineIndent = -1;
+
+      // must find previous line with content
+      for (let lineIndex = lineNumber - 2; lineIndex >= 0; lineIndex--) {
+        const indent = computeIndentLevel(lines[lineIndex], tabSize);
+        if (indent >= 0) {
+          aboveContentLineIndex = lineIndex;
+          aboveContentLineIndent = indent;
+          break;
+        }
+      }
+    }
+
+    if (
+      belowContentLineIndex !== -1 &&
+      (belowContentLineIndex === -2 || belowContentLineIndex < lineNumber - 1)
+    ) {
+      belowContentLineIndex = -1;
+      belowContentLineIndent = -1;
+
+      // must find next line with content
+      for (let lineIndex = lineNumber; lineIndex < lineCount; lineIndex++) {
+        const indent = computeIndentLevel(lines[lineIndex], tabSize);
+        if (indent >= 0) {
+          belowContentLineIndex = lineIndex;
+          belowContentLineIndent = indent;
+          break;
+        }
+      }
+    }
+
+    result[resultIndex] = getIndentLevelForWhitespaceLine_(
+      aboveContentLineIndent,
+      belowContentLineIndent
+    );
+  }
+
+  return result;
+};
+
 const getGuidesByLine = (
   lines: string[],
   indentSize: number,
@@ -213,6 +307,99 @@ const getGuidesByLine = (
   }
 
   return result;
+};
+
+const renderIndentGuides_ = (
+  fileBlobContainerElement: HTMLElement,
+  fileLineContainerElement: HTMLElement,
+  lines: string[],
+  indentSize: number,
+  tabSize: number
+): void => {
+  const lineCount = getLineCount(lines);
+  const height = lineCount * lineHeight;
+
+  const linesContentContainerElement = document.createElement('div');
+  linesContentContainerElement.className = 'lines-content';
+  linesContentContainerElement.style.position = 'relative';
+  linesContentContainerElement.style.width = '100%';
+  linesContentContainerElement.style.height = `${height}px`;
+  linesContentContainerElement.style.overflow = 'hidden';
+
+  const viewOverlayContainerElement = document.createElement('div');
+  viewOverlayContainerElement.className = 'view-overlays';
+  viewOverlayContainerElement.style.position = 'absolute';
+  viewOverlayContainerElement.style.top = '0px';
+  viewOverlayContainerElement.style.width = '5000px';
+  viewOverlayContainerElement.style.height = '0px';
+
+  const indentGuides = getLinesIndentLevels(lines, indentSize, tabSize);
+
+  for (let lineNumber = 1; lineNumber <= lineCount; lineNumber++) {
+    const lineElement = document.createElement('div');
+    lineElement.style.position = 'absolute';
+    lineElement.style.left = '0px';
+    lineElement.style.top = `${lineHeight * (lineNumber - 1)}px`;
+    lineElement.style.width = '100%';
+    lineElement.style.height = `${lineHeight}px`;
+
+    const lineIndex = lineNumber - 1;
+    const indent = indentGuides[lineIndex];
+    const indentLevelsInLine = Math.ceil(indent / indentSize);
+    const leftOffset = 60; // TODO: compute line number content width;
+
+    for (let indentLvl = 1; indentLvl <= indentLevelsInLine; indentLvl++) {
+      const indentGuide = (indentLvl - 1) * indentSize + 1;
+
+      const left = leftOffset + (indentGuide - 1) * spaceWidth;
+      const width = spaceWidth;
+
+      const verticalLineIndentGuideElement = document.createElement('div');
+      verticalLineIndentGuideElement.classList.add(
+        'core-guide',
+        'core-guide-indent',
+        'vertical'
+      );
+      verticalLineIndentGuideElement.style.position = 'absolute';
+      verticalLineIndentGuideElement.style.left = `${left}px`;
+      verticalLineIndentGuideElement.style.height = `${lineHeight}px`;
+      verticalLineIndentGuideElement.style.width = `${width}px`;
+      verticalLineIndentGuideElement.style.boxShadow = `1px 0 0 0 ${borderColor} inset`;
+      lineElement.appendChild(verticalLineIndentGuideElement);
+
+      if (!lines[lineIndex].length) {
+        continue;
+      }
+
+      const coloredIndentGuideElement = document.createElement('div');
+      coloredIndentGuideElement.classList.add(
+        'core-guide',
+        'core-guide-indent',
+        'colored'
+      );
+      coloredIndentGuideElement.style.position = 'absolute';
+      coloredIndentGuideElement.style.left = `${left}px`;
+      coloredIndentGuideElement.style.height = `${lineHeight}px`;
+      coloredIndentGuideElement.style.width = `${width * indentSize}px`;
+      const indentColor = colors[indentLvl % colors.length];
+      coloredIndentGuideElement.style.background = indentColor;
+      lineElement.appendChild(coloredIndentGuideElement);
+    }
+
+    viewOverlayContainerElement.appendChild(lineElement);
+  }
+
+  const viewLinesContainerElement = document.createElement('div');
+  viewLinesContainerElement.className = 'view-lines';
+  viewLinesContainerElement.style.position = 'absolute';
+  viewLinesContainerElement.appendChild(fileLineContainerElement);
+
+  linesContentContainerElement.append(
+    viewOverlayContainerElement,
+    viewLinesContainerElement
+  );
+
+  fileBlobContainerElement.append(linesContentContainerElement);
 };
 
 const renderIndentGuides = (
@@ -362,7 +549,7 @@ const onUpdate = async () => {
   const indentType = indent.type;
   const indentSize = indent.amount * (indentType === 'tab' ? tabSize : 1);
 
-  renderIndentGuides(
+  renderIndentGuides_(
     fileBlobContainerElement,
     fileLineContainerElement,
     lines,
